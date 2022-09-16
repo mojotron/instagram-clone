f3a2ab9f2300a978ab604d6524975cec9f3bb47a
mojo <igormalj@gmail.com>
2022-09-16 21:34:57 +0200

    Add project configuration and folder structure

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVJ82_OcSP7aKgrEGJHnvuW4OJYKVOcBs',
+  authDomain: 'instagram-clone-c8823.firebaseapp.com',
+  projectId: 'instagram-clone-c8823',
+  storageBucket: 'instagram-clone-c8823.appspot.com',
+  messagingSenderId: '803963430553',
+  appId: '1:803963430553:web:6e84f018f45d0719d3fa08',
+};
