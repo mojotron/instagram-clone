9519a9655a9309cf7ce8d35295a1b79183e5d2ed
mojo <igormalj@gmail.com>
2022-12-05 22:39:46 +0100

    Add structure and style for SearcBar

--- src/pages/Dashboard/components/Header.js
+++ src/pages/Dashboard/components/Header.js
@@ -1,13 +1,17 @@
 import './styles/Header.css';
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 const Header = ({ toggleShowCreatePost }) => {
   return (
     <header className="Header">
-      <Link to="/">
-        <h1>Instagram Clone</h1>
-      </Link>
+      <div className="Header__left">
+        <Link to="/">
+          <h1>Instagram Clone</h1>
+        </Link>
+        <SearchBar />
+      </div>
       <Navbar toggleShowCreatePost={toggleShowCreatePost} />
     </header>
   );
--- src/pages/Dashboard/components/SearchBar.js
+++ src/pages/Dashboard/components/SearchBar.js
@@ -1,7 +1,65 @@
+import { useEffect, useState } from 'react';
+import './styles/SearchBar.css';
+// icons
+import closeIcon from '../../../images/close-icon.svg';
+import searchIcon from '../../../images/search-icon.svg';
+
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [focus, setFocus] = useState(false);
+
+  console.log(searchTerm);
+  // save recent search terms
+  // focus on click
+
+  // useEffect(() => {
+  //   if (searchTerm === '') return;
+  //   const debounce = setTimeout(() => {
+  //     console.log(searchTerm);
+  //   }, 2000);
+
+  //   return () => clearTimeout(debounce);
+  // }, [searchTerm]);
+
+  const handleResetSearch = e => {
+    // e.stopPropagation();
+    console.log('reset');
+    setSearchTerm('');
+    setFocus(false);
+  };
+
   return (
-    <div>
-      <input type="text" />
+    <div className="SearchBar">
+      <form className="SearchBar__form">
+        <label>
+          {!focus && (
+            <img
+              src={searchIcon}
+              alt="search bar"
+              className="SearchBar__form__search-icon"
+            />
+          )}
+
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            className={focus ? 'active' : ''}
+            onChange={e => setSearchTerm(e.target.value)}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            // onClick={e => e.stopPropagation()}
+          />
+        </label>
+        {searchTerm !== '' && (
+          <img
+            src={closeIcon}
+            alt="search bar"
+            className="SearchBar__form__close-icon"
+            onClick={handleResetSearch}
+          />
+        )}
+      </form>
     </div>
   );
 };
