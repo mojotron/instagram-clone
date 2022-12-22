7a701338472339d2bee91da73a8506c4ba950508
mojo <igormalj@gmail.com>
2022-12-22 18:01:58 +0100

    Add hook for infinite scroll in explore page

---
+++ src/hooks/useCollectPostsWithLimit.js
@@ -0,0 +1,56 @@
+import { projectFirestore } from '../firebase/config';
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from 'firebase/firestore';
+import { useState } from 'react';
+
+export const useCollectPostsWithLimit = () => {
+  const [lastDocument, setLastDocument] = useState(null);
+  const [endOfDocuments, setEndOfDocuments] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+
+  console.log(lastDocument);
+
+  const getNextPosts = async () => {
+    if (endOfDocuments) return -1;
+    if (isFetching) return -1;
+
+    console.log('called from hook');
+    try {
+      setIsFetching(true);
+      const colRef = collection(projectFirestore, 'posts');
+      const q = query(
+        colRef,
+        orderBy('createdAt', 'asc'),
+        startAfter(lastDocument || 0),
+        limit(6)
+      );
+      const data = await getDocs(q);
+
+      if (data.empty) {
+        setEndOfDocuments(true);
+        return -1;
+      }
+
+      const result = [];
+      let last;
+      data.docs.forEach(doc => {
+        result.push({ ...doc.data(), id: doc.id });
+        last = doc;
+      });
+      setLastDocument(last);
+      setIsFetching(false);
+
+      return result;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return { getNextPosts };
+};
--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+// styles
+import './styles/Explore.css';
 // hooks
-import { useCollectPosts } from '../../hooks/useCollectPosts';
+import { useCollectPostsWithLimit } from '../../hooks/useCollectPostsWithLimit';
 // components
 import PostCard from '../Profile/components/PostCard';
 
 const Explore = () => {
-  return <div>Explore</div>;
+  const { getNextPosts } = useCollectPostsWithLimit();
+  const [posts, setPosts] = useState([]);
+  const containerRef = useRef();
+
+  console.log(posts);
+
+  useEffect(() => {
+    console.log('try to catch');
+    if (posts.length > 0) return;
+    console.log('catching');
+    getNextPosts().then(data => {
+      if (data !== -1) {
+        setPosts(data);
+      }
+    });
+  }, [getNextPosts, posts]);
+
+  const handleScroll = () => {
+    const triggerHeight =
+      containerRef.current.offsetHeight + containerRef.current.scrollTop;
+    if (triggerHeight >= containerRef.current.scrollHeight) {
+      console.log('LOUNCH');
+      getNextPosts().then(data => {
+        if (data !== -1) {
+          setPosts(oldValue => [...oldValue, ...data]);
+        }
+      });
+    }
+  };
+
+  return (
+    <div className="Explore">
+      <div
+        ref={containerRef}
+        className="Explore__posts"
+        onScroll={handleScroll}
+      >
+        {posts.map(post => (
+          <PostCard key={post.id} data={post} dimensions={post.dimensions} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Explore;
