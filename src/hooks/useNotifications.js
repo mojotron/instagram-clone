2d7ebabfd2573b8c46c6642722b415fce10f0125
mojo <igormalj@gmail.com>
2022-12-25 19:07:16 +0100

    create pointers for useNotification hook, marry x-mas

---
+++ src/hooks/useNotifications.js
@@ -0,0 +1,17 @@
+import {} from 'firestore/firebase';
+// hooks
+import { projectFirestore } from '../firebase/config';
+import { useUserDataContext } from './useUserDataContext';
+
+export const useNotifications = () => {
+  // types of notifications
+  // like post
+  // mention in comment
+  // new follower
+  // make notification and push it to target user notification array
+  // methods
+  // - create notification (for first notification user gets)
+  // - add notification
+  // - delete notification
+  // - collect notifications => create new hook
+};
