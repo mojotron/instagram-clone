a97ec521a8be34e40e18170e43c126d5989a5467
mojo <igormalj@gmail.com>
2023-01-07 22:36:18 +0100

    continue refactoring search

--- src/pages/Dashboard/components/SearchBar.js
+++ src/pages/Dashboard/components/SearchBar.js
@@ -1,13 +1,53 @@
+import { useState, useEffect, useRef } from 'react';
+// styles
 import './styles/Search.css';
 // icons
 import { IoIosCloseCircle } from 'react-icons/io';
+import { BsSearch } from 'react-icons/bs';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [hasFocus, setHasFocus] = useState(false);
+  const inputRef = useRef();
+
+  console.log('Focus is', hasFocus ? 'on' : 'off');
+
+  useEffect(() => {
+    // debounce search call, call search one and half second after user
+    // stops typing
+    if (searchTerm === '') return;
+    const debounce = setTimeout(() => {
+      console.log(searchTerm);
+    }, 1500);
+
+    return () => clearTimeout(debounce);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleClearSearch = () => {};
+
   return (
     <form className="SearchBar">
       <label>
-        <input type="text" />
-        <IoIosCloseCircle size={15} />
+        {!hasFocus && <BsSearch size={15} />}
+        <input
+          ref={inputRef}
+          type="text"
+          value={searchTerm}
+          placeholder="Search..."
+          onChange={e => setSearchTerm(e.target.value)}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+        />
+
+        {hasFocus && (
+          <button className="btn">
+            <IoIosCloseCircle size={15} color={'var(--gray)'} />
+          </button>
+        )}
       </label>
     </form>
   );
--- src/pages/Dashboard/components/SearchResults.js
+++ src/pages/Dashboard/components/SearchResults.js
@@ -1,12 +1,58 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// styles
 import './styles/Search.css';
+// icons
+import { GrClose } from 'react-icons/gr';
+// components
+import Avatar from '../../../components/Avatar';
+import { useEffect } from 'react';
+
+const SearchUserCard = ({ data, recent }) => {
+  return (
+    <div className="SearchUserCard">
+      <div className="SearchUserCard__wrapper">
+        <Avatar url={data.avatar.url} size="small" />
+        <div className="SearchUserCard__wrapper__name">
+          <h2>{data.userName}</h2>
+          <h3>{data.fullName}</h3>
+        </div>
+      </div>
+
+      {recent && (
+        <button className="btn">
+          <GrClose size={15} />
+        </button>
+      )}
+    </div>
+  );
+};
 
 const SearchResults = () => {
+  const navigate = useNavigate();
+
+  const [recentSearch, setRecentSearch] = useState(
+    JSON.parse(localStorage.getItem('instagram-clone-recent-search')) || []
+  );
+
+  const handleClearAllRecent = () => {};
+
+  const handleRemoveRecent = () => {};
+
+  useEffect(() => {}, []);
+
   return (
     <div className="SearchResults">
       <header className="SearchResults__header">
         <h3>Recent</h3>
         <button className="btn btn--blue">Clear all</button>
       </header>
+      <div className="SearchResults__container">
+        {recentSearch.length > 0 &&
+          recentSearch.map(user => (
+            <SearchUserCard data={user} recent={true} />
+          ))}
+      </div>
     </div>
   );
 };
